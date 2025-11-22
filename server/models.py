51ffaa99273a6c034cd75a1baecd1df8eb308dff
Gerry <gerrywiegand@gmail.com>
2025-11-22 12:53:53 -0500

    created exercise model

--- server/models.py
+++ server/models.py
@@ -4,3 +4,13 @@ from sqlalchemy.orm import validates  # noqa: F401
 db = SQLAlchemy()
 
 # Define Models here
+
+
+class Exercise(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    category = db.Column(db.String(80), nullable=False)
+    equipment_needed = db.Column(db.boolean, default=False)
+
+    def __repr__(self):
+        return f"<Exercise {self.name}>"
