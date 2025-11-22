e481c003ea0779ae4099332c475770ba63ac80c5
Gerry <gerrywiegand@gmail.com>
2025-11-22 12:56:54 -0500

    added tablenames

--- server/models.py
+++ server/models.py
@@ -7,6 +7,8 @@ db = SQLAlchemy()
 
 
 class Exercise(db.Model):
+    __tablename__ = "exercise"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     category = db.Column(db.String(80), nullable=False)
@@ -17,7 +19,18 @@ class Exercise(db.Model):
 
 
 class Workout(db.Model):
+    __tablename__ = "workout"
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(Date, nullable=False)
     duration_minutes = db.Column(db.Integer, nullable=False)
     notes = db.Column(db.String(200))
+
+
+class WorkoutExercise(db.Model):
+    __tablename__ = "workout_exercise"
+    id = db.Column(db.Integer, primary_key=True)
+    workout_id = db.Column(db.Integer, db.ForeignKey("workout.id"), nullable=False)
+    exercise_id = db.Column(db.Integer, db.ForeignKey("exercise.id"), nullable=False)
+    reps = db.Column(db.Integer, nullable=False)
+    sets = db.Column(db.Integer, nullable=False)
+    duration_seconds = db.Column(db.Integer, nullable=False)
