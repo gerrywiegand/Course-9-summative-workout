6283baa232e456f09260a052855eb8635e65adbd
Gerry <gerrywiegand@gmail.com>
2025-11-22 12:55:15 -0500

    created Workout model

--- server/models.py
+++ server/models.py
@@ -1,4 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy  # noqa: I001
+from flask_sqlalchemy import SQLAlchemy, Date  # noqa: I001
 from sqlalchemy.orm import validates  # noqa: F401
 
 db = SQLAlchemy()
@@ -14,3 +14,10 @@ class Exercise(db.Model):
 
     def __repr__(self):
         return f"<Exercise {self.name}>"
+
+
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(Date, nullable=False)
+    duration_minutes = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.String(200))
