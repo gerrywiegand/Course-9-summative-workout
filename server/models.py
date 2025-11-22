b08150f58457d00a5ac0aa6bdc652e428e5b82d8
Gerry <gerrywiegand@gmail.com>
2025-11-22 13:00:32 -0500

    added repr to all

--- server/models.py
+++ server/models.py
@@ -25,6 +25,9 @@ class Workout(db.Model):
     duration_minutes = db.Column(db.Integer, nullable=False)
     notes = db.Column(db.Text(200))
 
+    def __repr__(self):
+        return f"<Workout {self.date} - {self.duration_minutes} mins>"
+
 
 class WorkoutExercise(db.Model):
     __tablename__ = "workout_exercise"
@@ -34,3 +37,6 @@ class WorkoutExercise(db.Model):
     reps = db.Column(db.Integer, nullable=True)
     sets = db.Column(db.Integer, nullable=True)
     duration_seconds = db.Column(db.Integer, nullable=True)
+
+    def __repr__(self):
+        return f"<WorkoutExercise Workout ID: {self.workout_id}, Exercise ID: {self.exercise_id}>"
